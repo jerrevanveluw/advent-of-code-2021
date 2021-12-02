970e519bf7d653bbd3ae3e70b5f20b03e44d971a
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2021-12-02 10:53:34 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: Sequence<String>) = input
+        .map { it.toInt() }
+        .windowed(2)
+        .map { (first, second) -> first - second }
+        .filter { it < 0 }
+        .count()
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: Sequence<String>) = input
+        .map { it.toInt() }
+        .windowed(3)
+        .map { (a, b, c) -> a + b + c }
+        .windowed(2)
+        .map { (first, second) -> first - second }
+        .filter { it < 0 }
+        .count()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput.asSequence()) == 1)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input.asSequence()))
+    println(part2(input.asSequence()))
 }
