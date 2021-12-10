5bd22989dc00d9717cf605616e60eadf6c975449
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2021-12-10 17:30:08 +0100

    cleanup

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,9 +1,9 @@
-import Day10.Side.LEFT
-import Day10.Side.RIGHT
-import Day10.Type.Angled
-import Day10.Type.Block
-import Day10.Type.Curly
-import Day10.Type.Paren
+import Day10.Brace.Angled
+import Day10.Brace.Block
+import Day10.Brace.Curly
+import Day10.Brace.Paren
+import Day10.Brace.Type.LEFT
+import Day10.Brace.Type.RIGHT
 
 fun main() {
     Day10.all()
@@ -20,7 +20,7 @@ object Day10 : Day {
         mapNotNull { it.parse() }
             .flatten()
             .map {
-                when (it.type) {
+                when (it) {
                     is Angled -> 25137
                     is Block -> 57
                     is Curly -> 1197
@@ -32,91 +32,76 @@ object Day10 : Day {
 
     private val part2 = report {
         mapNotNull { it.parse(true) }
-            .map { it.autocomplete() }
-            .map { it.reversed() }
-            .map { it.calculateSum() }
+            .map(::autocomplete)
+            .map(::reversed)
+            .map(::calculateSum)
             .sorted()
-            .run { elementAt(count() / 2) }
+            .getMiddle()
     }
 
-    private fun List<Brace>.calculateSum() = fold(0L) { acc, brace ->
-        (acc * 5) + when (brace.type) {
-            is Curly -> 3L
-            is Paren -> 1L
-            is Block -> 2L
-            is Angled -> 4L
+    private fun <T> Sequence<T>.getMiddle() = elementAt(count() / 2)
+
+    private fun <T> reversed(iterable: Iterable<T>) = iterable.reversed()
+
+    private fun calculateSum(l: List<Brace>): Long = l.fold(0L) { acc, brace ->
+        acc * 5 + when (brace) {
+            is Curly -> 3
+            is Paren -> 1
+            is Block -> 2
+            is Angled -> 4
         }
     }
 
-    private fun List<Brace>.autocomplete() = mutableListOf<Brace>().let {
-        forEach { brace ->
-            when (brace.type.side) {
-                LEFT -> it.add(brace)
-                RIGHT -> it.removeLast().run {
-                    when {
-                        type is Curly && brace.type is Curly && type.side == LEFT -> true
-                        type is Paren && brace.type is Paren && type.side == LEFT -> true
-                        type is Block && brace.type is Block && type.side == LEFT -> true
-                        type is Angled && brace.type is Angled && type.side == LEFT -> true
-                        else -> false
-                    }
-                }
-            }
+    private fun autocomplete(braces: List<Brace>): List<Brace> {
+        val mutableList = mutableListOf<Brace>()
+        braces.forEach { mutableList.addUntilCorruptOrIncomplete(it) }
+        return mutableList
+    }
+
+    private fun List<Brace>.parse(filterCorrupt: Boolean = false): List<Brace>? {
+        val mutableList = mutableListOf<Brace>()
+        forEach { if (!mutableList.addUntilCorruptOrIncomplete(it)) return if (filterCorrupt) null else listOf(it) }
+        return if (filterCorrupt) this else null
+    }
+
+    private fun MutableList<Brace>.addUntilCorruptOrIncomplete(brace: Brace) = when (brace.type) {
+        LEFT -> add(brace)
+        RIGHT -> removeLast().let { it == brace && it.type == LEFT }
+    }
+
+    private sealed class Brace(val type: Type) {
+        class Curly(type: Type) : Brace(type)
+        class Paren(type: Type) : Brace(type)
+        class Angled(type: Type) : Brace(type)
+        class Block(type: Type) : Brace(type)
+
+        enum class Type { LEFT, RIGHT }
+
+        override fun equals(other: Any?): Boolean = when {
+            this is Curly && other is Curly -> true
+            this is Paren && other is Paren -> true
+            this is Angled && other is Angled -> true
+            this is Block && other is Block -> true
+            else -> false
         }
-        it
+
+        override fun hashCode() = type.hashCode()
     }
 
-    private fun List<Brace>.parse(filterCorrupt: Boolean = false) = mutableListOf<Brace>().let {
-        forEach { brace ->
-            val valid = when (brace.type.side) {
-                LEFT -> it.add(brace)
-                RIGHT -> it.removeLast().run {
-                    when {
-                        type is Curly && brace.type is Curly && type.side == LEFT -> true
-                        type is Paren && brace.type is Paren && type.side == LEFT -> true
-                        type is Block && brace.type is Block && type.side == LEFT -> true
-                        type is Angled && brace.type is Angled && type.side == LEFT -> true
-                        else -> false
-                    }
-                }
-            }
-            if (!valid) {
-                return@let if (filterCorrupt) null else listOf(brace)
-            }
+    private fun <R : Any> report(block: Sequence<List<Brace>>.() -> R) = setup(block) { map(::tokenize) }
+
+    private fun tokenize(string: String) = string.map { c ->
+        when (c) {
+            '{' -> Curly(LEFT)
+            '}' -> Curly(RIGHT)
+            '(' -> Paren(LEFT)
+            ')' -> Paren(RIGHT)
+            '[' -> Block(LEFT)
+            ']' -> Block(RIGHT)
+            '<' -> Angled(LEFT)
+            '>' -> Angled(RIGHT)
+            else -> throw RuntimeException("Illegal brace: '$c'")
         }
-        return@let if (filterCorrupt) this else null
-    }
-
-    private fun tokenize(string: String) = string.mapIndexed { idx, s ->
-        when (s) {
-            '{' -> Brace(idx, s, Curly(LEFT))
-            '}' -> Brace(idx, s, Curly(RIGHT))
-            '(' -> Brace(idx, s, Paren(LEFT))
-            ')' -> Brace(idx, s, Paren(RIGHT))
-            '[' -> Brace(idx, s, Block(LEFT))
-            ']' -> Brace(idx, s, Block(RIGHT))
-            '<' -> Brace(idx, s, Angled(LEFT))
-            '>' -> Brace(idx, s, Angled(RIGHT))
-            else -> throw RuntimeException("Parse error at index: $idx")
-        }
-    }
-
-    private data class Brace(val index: Int, val char: Char, val type: Type)
-
-    private enum class Side {
-        LEFT, RIGHT
-    }
-
-    private sealed class Type(val side: Side) {
-        class Curly(side: Side) : Type(side)
-        class Paren(side: Side) : Type(side)
-        class Angled(side: Side) : Type(side)
-        class Block(side: Side) : Type(side)
-    }
-
-
-    private fun <R : Any> report(block: Sequence<List<Brace>>.() -> R) = setup(block) {
-        map(::tokenize)
     }
 
 }
